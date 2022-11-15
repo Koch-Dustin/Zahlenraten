eda2a24c6698bd686eddeeba52553dde452ead36
Dustin Koch <dustin.koch@5minds.de>
2022-11-15 13:23:06 +0100

    Add early exit

--- main.py
+++ main.py
@@ -4,45 +4,45 @@ def game():
   numberToGuess = random.randint(1, 100) 
   trysTakeToGuess = 0
 
-  print("Ich denke an eine Zahl zwischen 1 - 20. Kannst du sie erraten?")
+  print("Ich denke an eine Zahl zwischen 1 - 100. Kannst du sie erraten?")
 
-  userGuessedNumber = -1
+  userGuessedNumber = 0
   
   while True:
     userGuessedNumber = input("Was denkst du: ")
-  
-    if(userGuessedNumber.isdigit()):
-      trysTakeToGuess = trysTakeToGuess + 1
-      
-      isOutOfRangeFrom50 = int(userGuessedNumber) < numberToGuess - 50 or int(userGuessedNumber) > numberToGuess + 50
-      isOutOfRangeFrom25 = int(userGuessedNumber) < numberToGuess - 25 or int(userGuessedNumber) > numberToGuess + 25
-      isOutOfRangeFrom10 = int(userGuessedNumber) < numberToGuess - 10 or int(userGuessedNumber) > numberToGuess + 10
-      isOutOfRangeFrom5 = int(userGuessedNumber) < numberToGuess - 5 or int(userGuessedNumber) > numberToGuess + 5
-      isOutOfRangeFrom2 = int(userGuessedNumber) < numberToGuess - 2 or int(userGuessedNumber) > numberToGuess + 2
-      
-      if(isOutOfRangeFrom50):
-        print("Sehr Kalt")
-      
-      elif(isOutOfRangeFrom25): 
-        print("Kalt")
-        
-      elif(isOutOfRangeFrom10):
-        print("Lauwarm")
-        
-      elif(isOutOfRangeFrom5):
-        print("Warm")
-        
-      elif(isOutOfRangeFrom2):
-        print("Sehr Warm")
-      else:
-        print("Klasse! Du hast meine Zahl erraten!")
-        print("Gebrauchte Versuche: " + str(trysTakeToGuess))
-        playAgain = input("Willst du nochmal spielen? [Y / N] : ")
-        if(playAgain == "Y"):
-          game()
-        break
     
-    else:
+    if(not userGuessedNumber.isdigit()):
       print("Es heißt nicht umsonst ZAHLEN Raten! Versuch es nochmal!")
+      break
+    
+    trysTakeToGuess = trysTakeToGuess + 1
+      
+    isOutOfRangeFrom50 = int(userGuessedNumber) < numberToGuess - 50 or int(userGuessedNumber) > numberToGuess + 50
+    isOutOfRangeFrom25 = int(userGuessedNumber) < numberToGuess - 25 or int(userGuessedNumber) > numberToGuess + 25
+    isOutOfRangeFrom10 = int(userGuessedNumber) < numberToGuess - 10 or int(userGuessedNumber) > numberToGuess + 10
+    isOutOfRangeFrom5 = int(userGuessedNumber) < numberToGuess - 5 or int(userGuessedNumber) > numberToGuess + 5
+    isOutOfRangeFrom2 = int(userGuessedNumber) < numberToGuess - 2 or int(userGuessedNumber) > numberToGuess + 2
+    
+    if(isOutOfRangeFrom50):
+      print("Sehr Kalt")
+    
+    elif(isOutOfRangeFrom25): 
+      print("Kalt")
+      
+    elif(isOutOfRangeFrom10):
+      print("Lauwarm")
+      
+    elif(isOutOfRangeFrom5):
+      print("Warm")
+      
+    elif(isOutOfRangeFrom2):
+      print("Sehr Warm")
+    else:
+      print("Klasse! Du hast meine Zahl erraten!")
+      print("Gebrauchte Versuche: " + str(trysTakeToGuess))
+      playAgain = input("Willst du nochmal spielen? [Y / N] : ")
+      if(playAgain == "Y"):
+        game()
+      break
 
 game()
